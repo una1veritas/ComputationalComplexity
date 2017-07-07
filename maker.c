5f9205edcfe7d6431a4e47294fe97507c8e9c20d
Sin Shimozono <una.veritas@icloud.com>
2017-07-07 18:28:59 +0900

    cpu

--- maker.c
+++ maker.c
@@ -9,30 +9,34 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	unsigned int a, i;
+	unsigned int a, i, h;
 
-	printf("stb.0.0 * _  stb.0.0 N 0 N\n");
+	/* Read only mem (input), A*/
 
-	printf("stb.0.0 L 0  ldi.0.0 R 0 N\n");
+	/* reset ... clear registers and rewind PC */
+	printf("#reset action\n");
+	printf("reset _ * 	stb.0 _ R 0 N\n");
+	for(i = 0; i < 10; i++)
+		printf("reset %d * 	reset %d L 0 N\n", i, i);
 
-	for (a = 0; a < 16; a++) {
-		printf("ldi.0.0 %X 0  stb.%X.0 R 0 N\n", a, a);
-	}
+	/* fetch instruction */
 
-	for (a = 0; a < 16; a++) {
-		printf("stb.%X.0 A 0  adi.%X.0 R 0 N\n", a, a);
-	}
+	printf("#halt\n");
+	printf("stb.0 _ * 	halt.0 _ N 0 N\n");
+	printf("stb.1 _ * 	halt.0 _ N 0 N\n");
+	printf("halt.0 * * 	halt.0 * N * N\n");
 
-	for (a = 0; a < 16; a++) {
-		for (i = 0; i < 16; i++ ) {
-			printf("adi.%X.0 %X 0  stb.%X.%X R 0 N\n", a, i, (a+i)&0xf, ( (a+i)>>4&1) );
-		}
-	}
+	printf("#load immediate\n");
+	printf("stb.0 l * 	ldi.0 l R 0 N\n");
+	printf("stb.1 l * 	ldi.0 l R 0 N\n");
+	for(i = 0; i < 10; i++)
+		printf("ldi.0 %d * 	stb.0 %d R %d N\n", i, i, i);
 
-	for (a = 0; a < 16; a++) {
-		printf("stb.%X.0 P 0  stb.%X.0 R %x R\n", a, a, a);
-		printf("stb.%X.1 P 0  stb.%X.1 R %x R\n", a, a, a);
-	}
+	printf("#add immediate with carry\n");
+	printf("stb.0 a * 	adc.0 a R * N\n");
+	for(a = 0; a < 10; a++)
+		for(i = 0; i < 10; i++)
+			printf("adc.0 %d %d 	stb.%d %d R %d N\n", i, i, a, (i+a > 9), (i+a)%10);
 
 	return EXIT_SUCCESS;
 }
