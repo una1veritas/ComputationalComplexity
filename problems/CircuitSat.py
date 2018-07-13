7af61b75712c2b398029e824d716a103f15c79c6
Sin Shimozono <una.veritas@icloud.com>
2018-07-13 17:11:10 +0900

    CircuitSat

--- problems/CircuitSat.py
+++ problems/CircuitSat.py
@@ -3,26 +3,29 @@ import random
 #from probinstance import CNFFormula
 
 class BooleanCircuit():
-    def __init__(self, cstr):
-        self.gateseq = eval(cstr)
-        self.inputs = sorted([gate for gate in self.gateseq if isinstance(gate, int) ])
-    
-    def evaluate(self, bits):
-        gatevals = [False, True]
-        for gi in range(2, len(self.gateseq)):
-            if gi in self.inputs :
-                if isinstance(bits[gi],bool) :
-                    gatevals[gi] = bits[gi]
-                else:
-                    gatevals[gi] = (int(bits[gi]) != 0)
+    def __init__(self, bc_str):
+        self.inputs = []
+        self.circuit = dict()
+        gateseq = eval(bc_str)
+        for i in range(0,len(gateseq)):
+            if isinstance(gateseq[i],int) :
+                self.inputs.append(i)
             else:
-                t_gate = self.gateseq[gi]
-                gatevals[gi] = not (gatevals[t_gate[0]] and gatevals[t_gate[1]])
+                self.circuit[i] = gateseq[i]
+    
+    def evaluate(self, assign):
+        gatevals = assign
+        for (i, pair) in self.circuit.items():
+            print(str(i)+str(pair))
+            if  not ((gatevals[pair[0]] == 1) and (gatevals[pair[1]] == 1)) :
+                gatevals[i] = 1
+            else:
+                gatevals[i] = 0
         print(gatevals)
-        return gatevals[-1]
+        return gatevals[len(gatevals)-1]
         
     def __str__(self):
-        return str(self.inputs) + ': ' + str(self.gateseq)
+        return str(self.inputs) + ': ' + str(self.circuit)
 
 #引数には python の論理式として書かれた3-CNF 論理式
 for argval in sys.argv[1:] :
@@ -33,11 +36,11 @@ print('Boolean Circuit '+str(cc))
 
 if 'deterministic' in sys.argv[1:]:
     bassign = { }
-    for bvar in f.variables:
+    for bvar in cc.inputs:
         bassign[bvar] = False
     while (True):
         print(str(sorted([assoc for assoc in bassign.items()])), end='')
-        if f.evaluate(bassign) :
+        if cc.evaluate(bassign) :
             print(' -> satisfied.')
         else:
             print(' -> not satisfied.')
@@ -49,11 +52,11 @@ if 'deterministic' in sys.argv[1:]:
                 break
             bassign[var] = False
 else:
-    bits = [i for i in range(0,len(cc.gateseq))]
-    for i in cc.inputs:
-        bits[i] = random.getrandbits(1)
-    print('assignment = ' + str(bits))
-    print(cc.evaluate(bits))
+    assign = dict()
+    for ig in cc.inputs:
+        assign[ig] = random.getrandbits(1)
+    print('assignment = ' + str(assign))
+    print(cc.evaluate(assign))
     
 '''
 python CircuitSat.py "[0,1,2,3,4,(2,3),(2,2),(4,6),(5,7)]"
